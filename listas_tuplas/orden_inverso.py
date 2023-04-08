52ddb9d2f221ea0007485184feea8cb00644d1c4
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-07 19:09:02 -0500

    Programa orden-inverso

---
+++ listas_tuplas/orden_inverso.py
@@ -0,0 +1,11 @@
+#Escribir un programa que almacene en una lista los números del 1 al 10
+#y los muestre por pantalla en orden inverso separados por comas.
+
+
+#Definimos la lista
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#con el comando reversed lo que estamos haciendo es invertir el orden de la lista
+inverso = list(reversed(lista))
+print(lista)
+print(inverso)
