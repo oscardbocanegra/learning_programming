3c597be9775157a80c83f9d96e32b0ea3fdb733e
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-07 20:13:05 -0500

    ejercicio de pago en juegos

---
+++ conditionals/pagos.py
@@ -0,0 +1,19 @@
+# Ejercicio 
+
+# Escribir un programa para una empresa que tiene salas de juegos para todas las edades
+# y quiere calcular de forma automática el precio que debe cobrar a sus clientes por entrar.
+# El programa debe preguntar al usuario la edad del cliente y mostrar el precio de la entrada. 
+# Si el cliente es menor de 4 años puede entrar gratis, si tiene entre 4 y 18 años debe pagar 
+# 5€ y si es mayor de 18 años, 10€.
+
+age = int(input("¿Cual es tu edad? "))
+print(age)
+
+if age < 4:
+    print("Tu edad te permite entrar a jugar gratis")
+
+elif age <= 18:
+    print("debes de pagar 5 Euros")
+    
+else:
+    print("Debes de pagar 10 Euros")
