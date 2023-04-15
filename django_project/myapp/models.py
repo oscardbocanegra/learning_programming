6cf76cd53278198575d5db923a7dfad05dc364c3
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-15 17:40:35 -0500

    Calse de interaccion de python con la base de datos

--- django_project/myapp/models.py
+++ django_project/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+class Task(models.Model):
+    tittle = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
