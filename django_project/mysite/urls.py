1c92647b88e78d8c000ae849b7f2cdc0006c5e77
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-14 21:56:41 -0500

    Agregamos myapp dentro de proyecto Django

--- django_project/mysite/urls.py
+++ django_project/mysite/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myapp import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello),
-    path('about/', views.about),
+    path('', include('myapp.urls'))
 ]
