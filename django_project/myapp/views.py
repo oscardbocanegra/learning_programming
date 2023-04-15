a5d8658e843d509988c68b8615bc8c2038827f28
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-14 21:44:14 -0500

    Usamos archivo urls.py para primer hello_world

---
+++ django_project/myapp/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello world</h1>")
+
+def about(request):
+    return HttpResponse ("About")
