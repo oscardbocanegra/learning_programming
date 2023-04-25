e764c22be268e58f9763e2822c37ce715631b45b
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-25 14:21:25 -0500

    registro de usuarios

--- django_project2/tasks/views.py
+++ django_project2/tasks/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'signup.html', {
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    
+    if request.method == 'GET':
+        return render (request, 'signup.html', {
         'form': UserCreationForm
     })
+    
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            User.objects.create_user(username=request.POST['username'])
+        return HttpResponse('Password do not match')
