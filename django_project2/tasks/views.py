b41a94c5cad7e7bf05b3027d23151be00752130d
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-25 16:26:04 -0500

    Templates y condicionales

--- django_project2/tasks/views.py
+++ django_project2/tasks/views.py
@@ -1,20 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.http import HttpResponse
+from django.db import IntegrityError
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
+
 def signup(request):
-    
+
     if request.method == 'GET':
-        return render (request, 'signup.html', {
-        'form': UserCreationForm
-    })
-    
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+
     else:
         if request.POST['password1'] == request.POST['password2']:
-            User.objects.create_user(username=request.POST['username'])
-        return HttpResponse('Password do not match')
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                'form': UserCreationForm,
+                "error": 'User alredy exists'
+            })
+                
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            "error": 'Password do not match'
+        })
+
+def tasks(request):
+    return render(request, 'tasks.html')
