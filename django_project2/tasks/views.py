b8b26edf929315982f03dfa4e1341665ef340ecf
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-25 17:38:35 -0500

    Uso de longing and longout

--- django_project2/tasks/views.py
+++ django_project2/tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 from django.db import IntegrityError
 
@@ -29,14 +29,40 @@ def signup(request):
                 return redirect('tasks')
             except IntegrityError:
                 return render(request, 'signup.html', {
-                'form': UserCreationForm,
-                "error": 'User alredy exists'
-            })
-                
+                    'form': UserCreationForm,
+                    "error": 'User alredy exists'
+                })
+
         return render(request, 'signup.html', {
             'form': UserCreationForm,
             "error": 'Password do not match'
         })
 
+
 def tasks(request):
     return render(request, 'tasks.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+            
+        else:
+            login(request, user)
+            return redirect('tasks')
