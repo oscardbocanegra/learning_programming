56f0210b31da35933e1c28c5a4c3d3df3e641503
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-26 18:57:59 -0500

    Agregando tareas desde el admin

--- django_project2/tasks/models.py
+++ django_project2/tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models. DateTimeField(auto_now_add=True)
+    datecompleted = models.DateField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
