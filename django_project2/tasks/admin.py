efe533fe5f7e727f04c9743375830f3fb247c27e
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-04-27 15:30:44 -0500

    Crendo la clase para visualizar la fecha de creacion

--- django_project2/tasks/admin.py
+++ django_project2/tasks/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Task
 
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", )
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
