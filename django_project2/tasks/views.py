a94cc9e769b1f5cc446eaed5360404e3e75de65c
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-05-04 21:08:26 -0500

    Eliminar tarea

--- django_project2/tasks/views.py
+++ django_project2/tasks/views.py
@@ -76,6 +76,9 @@ def task_detail(request, task_id):
             return redirect('tasks')
         except ValueError:
             return render (request, 'task_detail.html', {'task': task, 'form': form, 'error': "Error updating task"})
+ 
+def complete_task(request, task_id):
+    get_object_or_404(Task, pk=task_id, user=request.user)
    
 def signout(request):
     logout(request)
