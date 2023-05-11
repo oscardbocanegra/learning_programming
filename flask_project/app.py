a4b56796b6208ad99d43baadd720d6e675287b6b
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-05-10 21:01:50 -0500

    Adding data base and making solutions in decorators

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,21 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'user'
 app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'localhost'
-mysql = MySQL()
+app.config['MYSQL_DB'] = 'flaskcontacts'
+mysql = MySQL(app)
 
 @app.route('/')
 def Index():
-    return 'Hello world'
+    return render_template('index.html')
 
 
-@app.route('/add_contact')
+@app.route('/add_contact', methods = ['POST'])
 def add_contact():
-    return 'Add contact'
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        print(fullname)
+        print(phone)
+        print(email)
+        return 'recived'
 
 
 @app.route('/edit')
