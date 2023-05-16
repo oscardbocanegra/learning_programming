3782ed9472ab9d3d089869c715f1abf72fc98a3b
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-05-16 17:52:49 -0500

    Doing django project 2 with some corrections

--- django_project2/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-class TaskAdmin(admin.ModelAdmin):
-    readonly_fields = ("created", )
-
-# Register your models here.
-admin.site.register(Task, TaskAdmin)
