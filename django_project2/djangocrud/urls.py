28399d1dedc70db72b487e382e8e856fb5e4ce12
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-05-17 21:02:09 -0500

    Validations of passwords

--- django_project2/djangocrud/urls.py
+++ django_project2/djangocrud/urls.py
@@ -20,7 +20,7 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloworld),
-    path('signup/', views.helloworld),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
     
 ]
--- django_project2/tasks/views.py
+++ django_project2/tasks/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+def home(request):
+    return render(request, 'home.html',)
+
+def signup(request):
+    
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username= request.POST['username'], password=request.POST['password1'])
+            user.save()
+            return HttpResponse ('User created successfuly')
+        return HttpResponse('Password do not match')
