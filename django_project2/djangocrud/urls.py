d70ff252c46c7350ad1ad294b6b906148449a923
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-05-17 20:41:11 -0500

    Agregando tabla de formulario

--- django_project2/djangocrud/urls.py
+++ django_project2/djangocrud/urls.py
@@ -21,5 +21,6 @@ from tasks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.helloworld),
+    path('signup/', views.helloworld),
     
 ]
--- django_project2/tasks/views.py
+++ django_project2/tasks/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def helloworld(request):
-    return render(request, 'signup.html')
+    return render(request, 'signup.html', {
+        'form': UserCreationForm
+    })
