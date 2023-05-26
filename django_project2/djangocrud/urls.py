d2b13f146fcc13d44520f10681b589843d16c51a
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-05-25 20:23:19 -0500

    Login done

--- django_project2/djangocrud/urls.py
+++ django_project2/djangocrud/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('tasks/', views.tasks, name='tasks'),
     path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
     
 ]
