0e52ce26cc54544c9c2f01eb56582bf21d3bb834
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-05-26 19:15:20 -0500

    Formulario de tareas con error

--- django_project2/tasks/views.py
+++ django_project2/tasks/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 from django.db import IntegrityError
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -41,6 +42,18 @@ def tasks(request):
     return render(request, 'tasks.html')
 
 
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
+    else:
+        print(request.POST)
+        return render(request, 'create_task.html', {
+        'form': TaskForm
+    })
+        
+
 def signout(request):
     logout(request)
     return redirect('home')
@@ -62,4 +75,3 @@ def signin(request):
         else:
             login(request, user)
             return redirect('tasks')
-
