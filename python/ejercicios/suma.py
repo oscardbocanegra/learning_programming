d9afc6b6dc9d0511abef40bfa0eb918e05391548
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-07-19 21:34:41 -0500

    ejercicio en python

---
+++ python/ejercicios/suma.py
@@ -0,0 +1,7 @@
+def suma_de_dos_numeros():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    suma = num1 + num2
+    print(f"La suma de {num1} y {num2} es: {suma}")
+
+suma_de_dos_numeros()
