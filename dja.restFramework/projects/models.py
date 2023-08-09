d0b5d230f23c6d14315816aeabc4344641997f01
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-08-09 17:37:17 -0500

    iniciando en java con Udemy

--- dja.restFramework/projects/models.py
+++ dja.restFramework/projects/models.py
@@ -5,4 +5,4 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     technology = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
