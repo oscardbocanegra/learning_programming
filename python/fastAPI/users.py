65bdb65fb995c030fb8d77fcb788f0252f598444
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-10-05 18:46:38 -0500

    adding user

--- python/fastAPI/users.py
+++ python/fastAPI/users.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/users")
-async def users():
-    return "Place of users"
+async def usersjson():
+    return [{"name": "Oscar", "lastname": "Bocanegra", "url" : "https://github.com/oscardbocanegra"},
+            {"name": "David", "lastname": "Capera", "url" : "https://github.com/davidcapera"},]
