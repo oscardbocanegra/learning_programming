d180ea95de2d185fa431957acdc7848181875e2b
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-10-08 19:54:07 -0500

    usersList

--- python/fastAPI/users.py
+++ python/fastAPI/users.py
@@ -3,7 +3,23 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/users")
+class User(BaseModel):
+    name: str
+    lastname: str
+    url: str
+    age: int
+    
+users_list = [User(name = "Oscar", lastname = "Bocanegra", url = "https://github.com/oscardbocanegra", age = 19 ), 
+              User(name = "David", lastname = "Capera", url = "https://github.com/davidcapera", age = 22),
+              User(name = "Juan", lastname = "Lopez", url = "https://github.com/juanlopez", age = 45)]
+
+@app.get("/usersjson")
 async def usersjson():
-    return [{"name": "Oscar", "lastname": "Bocanegra", "url" : "https://github.com/oscardbocanegra"},
-            {"name": "David", "lastname": "Capera", "url" : "https://github.com/davidcapera"},]
+    return [{"name": "Oscar", "lastname": "Bocanegra", "url" : "https://github.com/oscardbocanegra", "age":19},
+            {"name": "David", "lastname": "Capera", "url" : "https://github.com/davidcapera", "age":22},
+            {"name": "Juan", "lastname": "Lopez", "url" : "https://github.com/juanlopez", "age":45}]
+    
+    
+@app.get("/users")
+async def users():
+    return users_list
