359f31cc9c326292186ef9df842ab1f0d39182c1
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-10-11 17:47:55 -0500

    Using the put method

--- python/fastAPI/users.py
+++ python/fastAPI/users.py
@@ -38,8 +38,23 @@ async def user(user: User):
     if type(search_user(user.id)) == User:
         return {"error": "El usuario ya existe"}
     
+    users_list.append(user)
+    return user
+
+@app.put("/user/")
+async def user(user: User):
+    
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+            
+    if not found:
+        return {"error": "No se ha actualizado el usuario"}
     else:
-        users_list.append(user)
+        return user
 
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list) 
