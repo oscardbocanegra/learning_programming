faaf159f39e9988a7e8caeea0bd291feb3720b9a
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-10-15 17:46:15 -0500

    driving the issues with the differents HTTP response

--- python/fastAPI/users.py
+++ python/fastAPI/users.py
@@ -37,7 +37,7 @@ async def user(id: int):
 @app.post("/user/", status_code=201)    
 async def user(user: User): 
     if type(search_user(user.id)) == User:
-        return HTTPException(status_code=204, detail="El usuario ya existe")   
+        raise HTTPException(status_code=404, detail="El usuario ya existe")   
     
     users_list.append(user)
     return user
