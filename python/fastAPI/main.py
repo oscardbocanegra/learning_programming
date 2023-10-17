f446feccffb125a396d96fb40c1c09a3e4b7ab59
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-10-16 20:58:23 -0500

    Recurso statico

--- python/fastAPI/main.py
+++ python/fastAPI/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from routers import products, users
+from fastapi.staticfiles import StaticFiles  
 
 app = FastAPI()
 
 app.include_router(products.router)
 app.include_router( users.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
