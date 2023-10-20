e70294ed02cd4639ec61db5cec5f7e70ff7eae74
Oscar Bocanegra <davidbocanegrac@gmail.com>
2023-10-20 11:31:09 -0500

    continue with API authentication

--- python/fastAPI/routers/basic_auth_users.py
+++ python/fastAPI/routers/basic_auth_users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -35,3 +35,27 @@ users_db = {
 def search_user(username: str):
     if username in users_db:
         return UserDB(users_db[username])
+    
+async def current_user(token: str = Depends(oauth2)):
+    user = search_user(token)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Credenciales de autenticacion invalidas", 
+                            headers={"WWW-Authenticate": "Bearer"})
+    return user 
+    
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(status_code=400, detail="El usuario no es correcto")
+    
+    user = search_user(form.username)
+    if not form.password == user.password:
+        raise HTTPException(status_code=400, detail="La contraseña no es correcto")
+    
+    return {"access-token": user.username, "token_type": "bearer"}
+
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
