06da28b8032e191627f5fadd597efc7a2e0ce8f6
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-02-13 21:56:51 -0500

    learning js

---
+++ learning_js/01-tipos/01-variables.js
@@ -0,0 +1,14 @@
+let nombre = "Hola Mundo"
+// En las siguientes lineas de codigo vamos a mostar las diferentes formas de llamar a 
+// las variables tomando en cuenta los metodos UpperCamelCase y camelCase,
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+console.log(nombre);
+
+//Y en el siguiente codigo mostramos que si creamos la variable, mas adelante le vamos 
+//a poder indicar un valor sin idicarle el "let"
+
+let apellido;
+let animal;
+apellido = "Bocanegra"
