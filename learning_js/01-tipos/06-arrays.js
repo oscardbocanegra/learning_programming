a4b014fa49caf272a36038cfb88a43864668ffdc
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-02-16 21:20:11 -0500

    arrays

---
+++ learning_js/01-tipos/06-arrays.js
@@ -0,0 +1,11 @@
+// para crear un array lo podemos hacer de la siguiente manera
+let animales = ['perro', 'gato'];
+console.log(animales);
+
+// Ahora si queremos consultar el valor en una poscicion indicada lo
+// hacemos de la siguiente manera
+console.log(animales[0]);
+
+//Ahora si queremos agregar datos a dicho array lo indicamos asi:
+animales[2] = 'dragon'
+console.log(animales)
