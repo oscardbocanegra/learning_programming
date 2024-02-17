13a1734924395c25b54b3a90e3c46c446d5a4d7e
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-02-16 19:09:48 -0500

    Data Types

---
+++ learning_js/01-tipos/03-constantes.js
@@ -0,0 +1,10 @@
+let nombre = "Hola mundo";
+nombre = "Hola David"
+
+console.log(nombre);
+//Esto lo podemos hacer gracias a que con let es posible realizar este tipo de cambios
+
+//Pero si no queremos que esto suceda y que un valor sea fijo, podriamos cambiar el let 
+//por const y ya no vamos a poder cambiar el valor de la variable
+
+const nombre2 = "Hola Oscar";
