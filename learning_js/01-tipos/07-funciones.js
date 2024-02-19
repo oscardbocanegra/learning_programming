3dc1e928a8df35f3116d5dc2f70dad3874033f61
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-02-19 18:49:44 -0500

    argumentos y parametros

---
+++ learning_js/01-tipos/07-funciones.js
@@ -0,0 +1,16 @@
+function saludar() {
+    console.log('Hola mundo')
+}
+
+saludar();
+
+function suma(){
+    return 2 + 2 
+}
+
+//This is one way to show on our console the result 
+console.log(suma());
+
+//And this is another option to obtain the same result
+let resultado = suma();
+console.log(resultado); 
---
+++ learning_js/01-tipos/08-argumentos.js
@@ -0,0 +1,7 @@
+function suma(a, b){
+    return a + b;
+}
+
+let resultado = suma(5, 8);
+console.log(resultado);
+console.log(typeof suma)
