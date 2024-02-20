519af1fe7bc670d6b4c8662b9bd4415376cd5ba3
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-02-19 19:26:26 -0500

    operadores

---
+++ learning_js/02-operadores/01-artimeticos.js
@@ -0,0 +1,20 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'adicion'); // adicion
+// console.log(a - b, 'resta'); // resta
+// console.log(a * b, 'multiplicacion'); // multiplicacion
+// console.log(a / b, 'divicion'); // divicion
+// console.log(a % b, 'modulo'); // modulo
+// console.log(a ** b, 'potencia'); // potencia
+
+//incrementar
+console.log("Incrementar")
+console.log(++a)
+console.log(a++)
+console.log(a)
+// decrementar
+console.log("Decrementar")
+console.log(--a)
+console.log(a--)
+console.log(a)
