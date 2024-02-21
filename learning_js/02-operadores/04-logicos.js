400343edf64e169de9874cc921e73c567080a74a
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-02-20 21:17:21 -0500

    operadores

---
+++ learning_js/02-operadores/04-logicos.js
@@ -0,0 +1,18 @@
+// AND, OR NOT
+
+//AND &&
+let mayor = true;
+let suscrito = true;
+
+// console.log(true && true)
+// console.log(false && true)
+
+console.log('operador and', mayor && suscrito);
+
+// or ||
+
+console.log('operador or', mayor || suscrito);
+
+// NOT !
+console.log('operador not', !mayor);
+let catalogoInfantil = !mayor;
