4ff0b69b8d7d81e3b4be6d35a143bebaac6f1378
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-02-27 17:12:59 -0500

    control de flujo

---
+++ learning_js/03-control-flujo/10-switch.js
@@ -0,0 +1,12 @@
+let accion = 'guardar';
+
+switch(accion){
+    case 'listar':
+        console.log('Accion de listar');
+        break;
+    case 'guardar':
+        console.log('Accion de guardar');
+        break;
+    default:
+        console.log('Accion no reconocida');
+}
