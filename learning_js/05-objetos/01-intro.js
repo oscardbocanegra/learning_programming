dbeea758744ee18b61bf603a4ff2634fecd6c52f
Oscar Bocanegra <davidbocanegrac@gmail.com>
2024-03-02 16:42:53 -0500

    objects

---
+++ learning_js/05-objetos/01-intro.js
@@ -0,0 +1,13 @@
+let user = {
+    email : 'david@holamundo.io',
+    name : "David",
+    direccion: {
+        calle: "Queen st",
+        numero: 15
+    },
+    activo : true,
+    recuperarCave: function (){
+        console.log('Recuperando clave')
+    }
+}
+
