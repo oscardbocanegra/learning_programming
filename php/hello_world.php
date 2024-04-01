ae3c0470c1080ae2863293f4c27bee2e35a6818a
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-04-01 17:57:27 -0500

    listas

--- php/hello_world.php
+++ php/hello_world.php
@@ -16,7 +16,7 @@ echo $my_string . "\n";
 
 echo gettype($my_string) ."\n";
 
-$my_string = 6; // Tipado dinamico
+//$my_string = 6; // Tipado dinamico
 echo($my_string)."\n";
 echo gettype($my_string)."\n";
 
@@ -30,4 +30,29 @@ echo $my_int ."\n";
 echo $my_int + 2 . "\n";
 echo $my_int - 4 . "\n";
 
+
+$my_doble = 6.5 . "\n";
+echo $my_doble;
+echo gettype($my_doble)  . "\n";
+echo $my_int + $my_doble  . "\n";
+
+
+// booleanos
+$my_bool = true;
+echo $my_bool . "\n";
+echo gettype($my_bool) . "\n";
+
+
+echo "El valor de mi integer es $my_int y el de mi boolean es $my_bool". "\n";
+
+//Constantes
+
+const MY_CONSTANT = "El valor de la constante" . "\n";
+echo MY_CONSTANT . "\n"; 
+
+$my_array = [$my_string, $my_int, $my_doble];
+echo gettype($my_array) . "\n";
+echo $my_array[0] . "\n";
+array_push($my_array, $my_bool);
+print_r($my_array);
 ?>
