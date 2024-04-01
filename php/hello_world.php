e92d738ca8eb74f9a6d8f06e0dcc0800e0fa6c08
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-03-31 22:57:37 -0500

    variables con PHP

--- php/hello_world.php
+++ php/hello_world.php
@@ -1,3 +1,19 @@
 <?php 
-echo "Hola, PHP"
+
+// Esto es un comentario
+echo "Hola, PHP \n";
+
+/*
+Este es 
+un 
+Comentario
+*/
+
+// variables
+$my_string = "Esto es una cadena de texto";
+$my_string = "Aqui cambio el valor de la cadena de texto";
+echo $my_string . "\n";
+
+echo gettype($my_string);
+
 ?>
