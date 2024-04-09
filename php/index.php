a02bb65852cb64fd6931832857ff07207f8e3395
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-04-09 17:40:23 -0500

    types

--- php/index.php
+++ php/index.php
@@ -1,10 +1,24 @@
 <?php
 $name = "David";
+$isDev = true;
+$age = 44;
+
+var_dump($name);
+var_dump($isDev);
+var_dump($age);
+
+gettype($name);
+gettype($isDev);
+gettype($age);
+
+is_string($name);
+is_bool($isDev);
+is_int($age);
 ?>
 
 
 <h1>
-    <?= $name; ?>
+    <?= "Hola " . $name; ?>
 </h1>
 
 <style>
