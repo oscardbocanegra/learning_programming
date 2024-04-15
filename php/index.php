4055f5ceaa51c92d9e848edcf5fdde4d721d73de
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-04-14 22:40:47 -0500

    match

--- php/index.php
+++ php/index.php
@@ -1,7 +1,7 @@
 <?php
 $name = "David";
 $isDev = true;
-$age = 20;
+$age = 60;
 
 //TYPES
 
@@ -18,15 +18,30 @@ $age = 20;
 // is_int($age);
 
 $output = "Hola \"$name\", con una edad de $age";
+$output_age = match (true) {
+    $age < 2 => "Eres un bebé $name",
+    $age < 10 => "Eres un niño $name",
+    $age < 18 => "Eres un adolescente $name",
+    $age == 18 => "Eres mayor de edad $name",
+    $age < 50 => "Eres un adulto joven $name",
+    $age >= 50 => "Eres un adulto mayor $name",
+    default => "Ya eres más adulto que todos $name"
+};
+
+
 
 //CONSTANTES
-const NOMBRE = "Oscar";  
+//const NOMBRE = "Oscar";  
 ?>
 
 
 <h1>
     <?= $output; ?>
 </h1>
+
+<h2>
+<?= $output_age; ?>
+</h2>
 
 <style>
     :root {
