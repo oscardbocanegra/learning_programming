ef71f17401d73379ebe5682969990d2cd5ef2627
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-05-28 18:48:02 -0500

    java and python

---
+++ codigo_facilito/java/holaMundo/src/variables.java
@@ -0,0 +1,24 @@
+public class variables {
+
+    public static void main(String[] args) {
+        /*
+         *
+         *  4 types of data
+         *
+         *Integers
+         * Float
+         * Characters
+         * Boolean
+         *
+         * */
+
+        int version;
+        version = 9    ;
+        System.out.println( version);
+
+        int versionJava = 10;
+        System.out.println(versionJava);
+
+    }
+
+}
