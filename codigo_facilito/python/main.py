36a7d46f297f2e9348474f70931ee45a77b1fc21
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-05-28 19:07:58 -0500

    python class

---
+++ codigo_facilito/python/main.py
@@ -0,0 +1,14 @@
+username: str = "David"
+altura: float = 1.70
+edad:int = 20
+
+print(username, altura, edad)
+
+# LISTAS, TUPLAS Y DICCIONARIOS
+
+from typing import Tuple, List, Dict
+
+numbers: List[int] = [1, 2, 3]
+name: Tuple[str, str] = ("Oscar", "David")
+scores: Dict[str, int] = {"user1": 100, "User2":70}
+
