4c8eab393eb763ad1ae33f2d1de64e9fc4ea5bde
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-05-30 20:23:50 -0500

    functions

--- codigo_facilito/python/main.py
+++ codigo_facilito/python/main.py
@@ -12,3 +12,11 @@ numbers: List[int] = [1, 2, 3]
 name: Tuple[str, str] = ("Oscar", "David")
 scores: Dict[str, int] = {"user1": 100, "User2":70}
 
+# FUCTIONS
+
+def average(numbers: List[int]) -> float :
+    return sum(numbers) / len(numbers)
+
+scores: List[int] = [10,10,8,8,8,7]
+result: float =average(scores)
+print(f"The average is {result}")
