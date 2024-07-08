0bccea7dd031e11581f6bf524c259f232e8e8083
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-07-08 18:38:19 -0500

    functions

---
+++ codigo_facilito/javascript/funciones.js
@@ -0,0 +1,13 @@
+// Declaracion de una funcion
+
+function saludar(){
+    console.log("Hola mundo");
+}
+
+function cuadrado(numero){
+    return numero * numero;
+}
+
+let cuadrado_de_dos = cuadrado(2);
+console.log(cuadrado_de_dos);
+console.log(cuadrado(3))
