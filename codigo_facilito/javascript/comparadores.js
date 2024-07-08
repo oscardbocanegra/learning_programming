e387fb3471fee58ed152ce76bff5f4bdabed9925
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-07-07 21:18:04 -0500

    operadores de comparacion

---
+++ codigo_facilito/javascript/comparadores.js
@@ -0,0 +1,18 @@
+// 8 Operadores de comparacion
+
+/* 
+
+    ==  Igual 
+    === Estrictamente igual
+
+    != Desigualdad
+    !== Desigualdad estricta
+
+    x > y Mayor que
+    x < y Menor que 
+
+*/
+
+let edad = "24";
+
+let resultado = 24;
