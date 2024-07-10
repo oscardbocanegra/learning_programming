9777936075a6e11cecf80207c62ad110360f34ce
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-07-09 21:19:50 -0500

    POO Done

--- codigo_facilito/javascript/poo.js
+++ codigo_facilito/javascript/poo.js
@@ -16,3 +16,15 @@ curso.inscribir();
 let nombre = "David";
 let usuario = {nombre : nombre };
 console.log(usuario.nombre);
+
+// Duplicar o combinar objetos
+let user = {
+    edad: 20,
+    nombre: "David"
+}
+
+let esquemaPermisos = {nivel: 2}
+
+let copia = Object.assign(user, esquemaPermisos);
+
+console.log(copia)
