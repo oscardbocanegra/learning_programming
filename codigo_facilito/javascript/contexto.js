ae330bd1ae4b8b198ab2a77b774df1920ec0e0bd
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-08-08 19:37:14 -0500

    clases done

--- codigo_facilito/javascript/contexto.js
+++ codigo_facilito/javascript/contexto.js
@@ -26,3 +26,47 @@ let javaScript = new Curso("Curso profesional de JavaScript");
 
 console.log(javaScript.titulo);
 javaScript.inscribir()
+
+//Alcance de propiedades
+
+class Curso{
+    #title = "JavaScript"
+}
+
+let javaScript2 = new Curso();
+console.log(javaScript2.#title);
+
+
+// Metodo constructor
+
+class Curso{
+    constructor(titulo, color= "Yellow"){
+        this.titulo = titulo;
+        this.color = color;
+        console.log(arguments);
+    }
+}
+
+// Herencia de clases
+
+class User {
+    constructor(name){
+        this.name = name;
+    }
+    saludar(){console.log("Hola usuario")}
+}
+
+class Admin extends User{
+    constructor(name){
+        super(name);
+    }
+
+    saludar(){
+        super.saludar();
+
+        console.log("Hola soy admin")
+    }
+}
+
+let admin = new Admin("David")
+console.log(admin.name)
