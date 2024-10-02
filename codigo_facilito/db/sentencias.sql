791cb0a38f944b884c9eac19090909ab5a684858
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-10-02 17:57:19 -0500

    insert in db

--- codigo_facilito/db/sentencias.sql
+++ codigo_facilito/db/sentencias.sql
@@ -6,3 +6,9 @@ CREATE TABLE autores(
     fecha_nacimiento DATE, 
     pais_origen VARCHAR(40)
 );
+
+INSERT INTO autores (autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen)
+VALUES  (2, "David", "Capera", "M", "2004-04-20", "Colombia" ),
+        (3, "Juan", "Bocanegra", "M", "2014-08-16", "Mexico" ),
+        (4, "Luisa", "Raigoza", "F", "2005-02-12", "Argentina" ),
+        (5, "Aura", "Bocanegra", "M", "2003-08-30", "Polonia" );
