4daba03fa5dc24f0ff70177ccd73f94fec63640c
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-10-16 15:40:07 -0500

    variables primitivas

--- java2/appjava/src/Main.java
+++ java2/appjava/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        byte age = 18;
+        int bigNumber = 1000;
+        System.out.println(age);
+        System.out.println(bigNumber);
         System.out.println("Hello world!");
     }
 }
