9b7fdd1701476bd4dc970dfea1ceebfd80823d15
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-10-23 16:04:40 -0500

    operadores

--- java2/appjava/src/Main.java
+++ java2/appjava/src/Main.java
@@ -13,5 +13,21 @@ public class Main {
         System.out.println("\n Arrays");
         System.out.println(Arrays.toString(numbers));
 
+        // Operadores aritmeticos
+
+        int a = 2 + 2;
+        int b = 3 - 1;
+        int c = 2 * 2;
+        float d = 10f / 3f;
+        int e = 4 % 3;
+        System.out.println("\nOperators");
+        System.out.println("This is the result of 2 + 2 = " + a);
+        System.out.println("This is the result of 3 - 1 = " + b);
+        System.out.println("This is the result of 2 * 2 = " + c);
+        System.out.println("This is the result of 10 / 3 = " + d);
+        System.out.println("This is the result of 4 % 3 = " + e);
+
+
+
     }
 }
