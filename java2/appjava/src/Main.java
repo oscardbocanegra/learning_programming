83904f0b133e83a8214eb2d1c2b733acd0ea1a40
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-10-23 15:00:14 -0500

    Arrays

--- java2/appjava/src/Main.java
+++ java2/appjava/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         byte age = 18;
@@ -5,5 +7,11 @@ public class Main {
         System.out.println(age);
         System.out.println(bigNumber);
         System.out.println("Hello world!");
+
+        // Arrays
+        int[] numbers = {0, 1, 2, 3, 4};
+        System.out.println("\n Arrays");
+        System.out.println(Arrays.toString(numbers));
+
     }
 }
